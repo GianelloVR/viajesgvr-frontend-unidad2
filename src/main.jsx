88c6246c6e61e348b235e966832fd3c125f24521
Gianello Valenzuela Robin <gianello.valenzuela@usach.cl>
2026-06-17 23:25:14 -0400

    Servir frontend por HTTPS para Keycloak

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     authClient={keycloak}
     initOptions={{
       checkLoginIframe: false,
-      pkceMethod: false,
+      pkceMethod: "S256",
     }}
     isLoadingCheck={() => false}
   >
